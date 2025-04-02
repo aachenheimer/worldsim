a4fa25b2570863dca3bb8ba90881e0d6c03a45b0
aachenheimer <snake2eyes7@gmail.com>
2025-04-02 10:10:20 -0400

    Add files via upload

--- compsci.py
+++ compsci.py
@@ -1,5 +1,54 @@
 from pygame import *
+from random import *
 
+#general work
+
+#size of the world
+SIZE = 8 
+
+world = []
+
+class chunk:
+	def __init__(self, pos, data, borderTop, borderBottom, borderLeft, borderRight):
+		self.pos = pos
+		self.data = data
+		self.borderTop = borderTop
+		self.borderBottom = borderBottom
+		self.borderLeft = borderLeft
+		self.borderRight = borderRight
+
+	def draw_self(self, pos = (0,0)):
+		pass
+
+	def declare_position(self):
+		print(self.pos)
+
+class tile:
+	def __init__(self, pos, type, master, borderTop, borderBottom, borderLeft, borderRight):
+		self.pos = pos
+		self.type = type
+		self.master = master
+		self.borderTop = borderTop
+		self.borderBottom = borderBottom
+		self.borderLeft = borderLeft
+		self.borderRight = borderRight
+
+def populateWorld():
+	for yPos in range(SIZE):
+		for xPos in range(SIZE):
+			newChunk = chunk((xPos, yPos), [], (yPos == 0), (yPos == SIZE - 1), (xPos == 0), (xPos == SIZE - 1))
+			world.append(newChunk)
+
+def populateChunks():
+
+
+
+
+populateWorld()
+
+
+
+#Pygame work
 def draw_text(text = "test", fontParam = font.get_default_font(), size = 12, color = Color(255, 255, 255), pos = (0,0)):
 	if fontParam != font.get_default_font():
 		fontParam = font.match_font(fontParam)
@@ -13,7 +62,7 @@ SCREEN = display.set_mode()
 
 FONT = "bell"
 
-draw_text("hello world!", "bell", 144, Color(255, 255, 255), (0,0))
+#draw_text("hello world!", "bell", 144, Color(255, 255, 255), (0,0))
 
 display.update()
 
